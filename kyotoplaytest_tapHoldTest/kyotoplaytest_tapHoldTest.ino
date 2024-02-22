c07cbefe830ce26db0787f13c5391f5d0ce9cbf1
kaho です! <kaho@kahoabe.net>
2024-02-22 22:17:40 +0900

    got rid off extra stuff

--- kyotoplaytest_tapHoldTest/kyotoplaytest_tapHoldTest.ino
+++ kyotoplaytest_tapHoldTest/kyotoplaytest_tapHoldTest.ino
@@ -3,36 +3,21 @@
 /////////////////////VARIABLES///////////////////////
 const int initThreshold = 1014;  // when you want to start calculating values
 const int loopDelay = 100; //how long you want the delay at the end of the loop
-const int triggerEnd = 100;         //how long it might take
 const int tapHoldThreshold = 300;
-
-//threshholds for loop modes
-const int introThreshold = 1000;
-const int moodThreshold = 900;
-const int harmonicThreshold = 800;
-const int fillThreshold = 600;
-const int secretThreshold = 0;// 0 to 599
 
 //pins
 const int tablePin = A0; //the reading pin for the table
-const int seat1Pin = 22; //the pin for the cushion
-const int seat2Pin = 23; //the pin for the other cushion
 //const int resetPin = ; //the pin to reset the Arduino
 
-int seat1State = 0;
-int seat2State = 0; 
 short value = 0;
-bool lastContactState = false; 
 
 //timers
 SimpleTimer tapHoldTimer; 
 bool tapHoldFlag = false; 
-bool holdingFlag = false;
 
 void readMe(){ 
   value = analogRead(A0);
 }
-
 
 void tapHold() { 
   //NO CONTACT
@@ -62,7 +47,6 @@ void tapHold() {
     }
   }
 }
-
 
 void setup() { //happens once at the beginning of the program. 
   Serial.begin(115200);
